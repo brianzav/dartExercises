c1cfcb9978591b83e7388c59c15966c5f826b665
Brian Zavala <brianzav23@gmail.com>
2023-04-04 09:59:20 -0300

    first commit

---
+++ exercicios.dart
@@ -0,0 +1,90 @@
+void main() {
+  salarioMensual(150, 670);
+}
+
+void salarioMensual(num ganhoXHora, num horasTrabalhadasMes) {
+  num salarioFinal = ganhoXHora * horasTrabalhadasMes;
+  print('O seu salario final é: $salarioFinal');
+}
+
+
+
+
+// Faca um programa que receba 2 numeros e imprima a soma. 
+
+// void main() {
+//   somatoria(5,4);
+// }
+
+// void somatoria(int n1, int n2){
+//   int soma;
+//   soma = n1 + n2;
+//   print(soma);
+// }
+
+// faca um programa que peca as 4 notas bimestrais e mostra a media.
+
+// void main() {
+//   mediaFinal(5,3,10,9);
+// }
+
+// void mediaFinal(int nota1, int nota2, int nota3, int nota4){
+//   num mediaF;
+//   mediaF = (nota1+nota2+nota3+nota4)/4;
+//   print(mediaF);
+// }
+
+// faca um programa que converta metros para centimetros
+
+// void main() {
+//   conversor(3.7);
+// }
+
+// void conversor(num metros){
+//   num centimetros;
+//   centimetros = metros * 100;
+//   print('$metros m, equivale a : $centimetros cms');
+// }
+
+// faca um programa que peca o radio de um circulo, calcule e mostra sua area.
+
+// void main() {
+//   mostrarArea(4);
+// }
+
+// void mostrarArea(num radio){
+// num result;
+//   result = 3.14 * (radio*radio);
+//   print('A area do seu raio é, $result');
+// }
+
+
+// faca um programa que calcule a area de um quadrado, imediatamente mostre o dobro desta area para o usuario.
+
+// void main() {
+//   num area = calculaArea(4.5);
+  
+//   print(area*2);
+// }
+
+// num calculaArea(num ladoQuadrado){
+//   num area;
+//   area = ladoQuadrado * ladoQuadrado;
+//   return area;
+// }
+
+// faca um programa que pergunte quanto voce ganha por hora e um numero de horas trabalhados no mes, calcule e mostre o total do seu salario no mes.
+
+// void main() {
+//   salarioMensual(150, 670);
+// }
+
+// void salarioMensual(num ganhoXHora, num horasTrabalhadasMes){
+//  num salarioFinal = ganhoXHora * horasTrabalhadasMes;
+//   print('O seu salario final é: $salarioFinal');
+// }
+
+
+/*
+
+ */ 
